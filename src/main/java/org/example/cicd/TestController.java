40beb2c32522678d7d59b55955ad3e20f9ab4268
seoyoen22 <hsy2709@gmail.com>
2025-09-10 17:55:14 +0900

    test

--- src/main/java/org/example/cicd/TestController.java
+++ src/main/java/org/example/cicd/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "test06";
+        return "test07";
     }
 
     @GetMapping("/health")
