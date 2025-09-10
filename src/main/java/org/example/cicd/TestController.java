54298ef5e6cfd7416a3574298a5348dbfeba00d0
seoyoen22 <hsy2709@gmail.com>
2025-09-10 16:50:33 +0900

    gree-blue deploy

--- src/main/java/org/example/cicd/TestController.java
+++ src/main/java/org/example/cicd/TestController.java
@@ -1,5 +1,7 @@
 package org.example.cicd;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +10,10 @@ public class TestController {
     @GetMapping("/test")
     public String test(){
         return "test03";
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity health(){
+        return ResponseEntity.status(HttpStatus.OK).body("healthy");
     }
 }
